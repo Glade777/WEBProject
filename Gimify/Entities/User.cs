dbf86e30dc43cc141e37dcc424f78e0143aa515d
Glade777 <kyrychenkodaoff@gmail.com>
2025-02-24 23:52:13 +0200

    addet project

--- Gimify/Entities/User.cs
+++ Gimify/Entities/User.cs
@@ -3,8 +3,32 @@
     public class User
     {
         public int id { get; set; }
-        public string Username { get; set; }
+        public virtual string Username { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; } = false;
+    }
+
+    class Derivedclass : User
+    {
+        public string _username;
+
+        public override string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _username = value;
+                }
+                else
+                {
+                    _username = "Unknown";
+                }
+            }
+        }
     }
 }
