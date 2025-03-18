fb0e347f12dc5a8347e078aac61bfb32dfa8a48e
Glade777 <kyrychenkodaoff@gmail.com>
2025-03-18 14:20:22 +0200

    added control

---
+++ Gimify/Entities/PostController.cs
@@ -0,0 +1,27 @@
+﻿using Gimify.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gimify.DAL.Repositories
+{
+    public class PostRepository
+    {
+        private readonly Efcontext _context;
+
+        public PostRepository(Efcontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Posts>> GetPostsSortedByFavouriteCountAsync()
+        {
+            var posts = await _context.Posts
+                                       .OrderByDescending(p => p.FavouriteCount)
+                                       .ToListAsync();
+
+            return posts;
+        }
+
+    }
+}
