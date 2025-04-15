262300e76ff13407466361467f9d634b53755d6b
Glade777 <kyrychenkodaoff@gmail.com>
2025-04-15 14:17:01 +0300

    Json

---
+++ Gimify/Entities/IDataStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace Gimify.Entities
+{
+    public interface IDataStorage<T> where T : BaseEntity
+    {
+        List<T> GetAll();
+        void Add(T entity);
+        void Delete(int id);
+        T? GetById(int id);
+        void Update(T entity);
+        void Save();
+    }
+}
